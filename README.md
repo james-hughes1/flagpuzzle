af6acbc0be888f7a4e4d520924ee3cc547e5b5fc
James Hughes <jhughes2712@gmail.com>
2025-01-18 22:48:27 +0000

    Change: Strip back to an about page and a second page.

--- README.md
+++ README.md
@@ -18,5 +18,7 @@ More info coming soon...
  - **node.js & vite.** Enable Javascript code to be run on a server (and a local dev server)
     - Run `node server.js` to start the server, and establish connection to MongoDB.
     - Run `npm run dev` calls on vite, running the local dev server for testing.
- - **Express.**
+ - **Express.** Middleware.
+ - **MongoDB.** Database.
+ - **EJS.** Views.
 
--- server.js
+++ server.js
@@ -1,10 +1,5 @@
-// Hidden variables
-require('dotenv').config();
-
 const express = require("express");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
 const app = express();
 const PORT = 5000;
 
@@ -12,34 +7,23 @@ const PORT = 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-// MongoDB Connection
-mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}`);
-const nameSchema = new mongoose.Schema({ firstName: String });
-const Name = mongoose.model("Name", nameSchema);
-
 // Routes
 // Landing Page
 app.get('/', (req, res) => {
-    res.render('landing'); // Render landing.ejs
+    res.render('about'); // Render about.ejs
 });
 
-// Enter Name Page
-app.get('/enter-name', (req, res) => {
-    res.render('enter-name'); // Render enter-name.ejs
-});
-
-// Handle Form Submission
+// Handle Name Submission
 app.post('/submit-name', async (req, res) => {
-    const newName = new Name({ firstName: req.body.firstName });
-    await newName.save();
-    res.redirect('/names');
+    const username = req.body.username;
+    console.log(username)
+    res.redirect('/play');
     return;
 });
 
-// Display All Names
-app.get('/names', async (req, res) => {
-    const names = await Name.find();
-    res.render('names', { namesList: names }); // Render names.ejs with data
+// Play Page
+app.get('/play', (req, res) => {
+    res.render('play'); // Render play.ejs
 });
 
 // Start Server

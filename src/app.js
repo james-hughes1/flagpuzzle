8b2f22baf66a5ba33f6b3091801dca00fcc192d2
James Hughes <jhughes2712@gmail.com>
2025-01-19 00:14:43 +0000

    Feat: Figured out falling and stacking mechanism for single square blocks.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+let blockMove = 'none'
+
 // Function to create a grid
 function createGrid(numRows, numCols, cellStates) {
     const blockGrid = document.getElementById('blockGrid');
@@ -20,22 +22,68 @@ function createGrid(numRows, numCols, cellStates) {
 }
 
 function playGame() {
+    const numRows = 10;
+    const numCols = 8;
+
     // Initialisation
     let cellStates = [];
-    for (let row = 0; row < 20; row++) {
+    for (let row = 0; row < numRows; row++) {
         cellStates[row] = [];
-        for (let col = 0; col < 8; col++) {
-            if (Math.random() > 0.1 * col) {
-                cellStates[row][col] = 1;
-            } else {
-                cellStates[row][col] = 0;
-            }
+        for (let col = 0; col < numCols; col++) {
+            cellStates[row][col] = 0;
         }
     }
+    let falling = 1;
+    let fallingPosition = {row: 0, col: Math.floor(numCols * Math.random())};
+    cellStates[fallingPosition.row][fallingPosition.col] = 1;
 
     // Initial display
-    createGrid(20, 8, cellStates);
+    createGrid(numRows, numCols, cellStates);
+
+    // Game loop
+    intervalId = setInterval(() => {
+        if (falling === 0) {
+            fallingPosition = {row: 0, col: Math.floor(numCols * Math.random())};
+            cellStates[fallingPosition.row][fallingPosition.col] = 1;
+            falling = 1;
+        } else {
+            if (blockMove != 'none') {
+                if ((blockMove === 'left') && (cellStates[fallingPosition.row][(fallingPosition.col + numCols - 1) % numCols] === 0)) {
+                    cellStates[fallingPosition.row][fallingPosition.col] = 0;
+                    fallingPosition.col = (fallingPosition.col + numCols - 1) % numCols;
+                    cellStates[fallingPosition.row][fallingPosition.col] = 1;
+                    blockMove = 'none';
+                } else if ((blockMove === 'right') && (cellStates[fallingPosition.row][(fallingPosition.col + numCols + 1) % numCols] === 0)) {
+                    cellStates[fallingPosition.row][fallingPosition.col] = 0;
+                    fallingPosition.col = (fallingPosition.col + numCols + 1) % numCols;
+                    cellStates[fallingPosition.row][fallingPosition.col] = 1;
+                    blockMove = 'none';
+                }
+            }
+            if ((fallingPosition.row + 1 === numRows) || (cellStates[fallingPosition.row + 1][fallingPosition.col] === 1)) {
+                falling = 0;
+            } else {
+                cellStates[fallingPosition.row][fallingPosition.col] = 0;
+                fallingPosition.row++;
+                cellStates[fallingPosition.row][fallingPosition.col] = 1;
+            }
+        }
+
+        createGrid(numRows, numCols, cellStates);
+    }, 200);
 }
+
+// Add listeners for arrow key controls
+document.addEventListener('keydown', function(event) {
+    switch (event.key) {
+        case 'ArrowLeft':
+            blockMove = 'left';
+            break;
+        case 'ArrowRight':
+            blockMove = 'right';
+            break;
+    }
+});
 
 // Run game
 playGame();
